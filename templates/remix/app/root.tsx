7d6136b8c5d149eb88b125a49576e34023c3069f
jonathanprozzi <jonathanprozzi@gmail.com>
2024-03-18 22:12:32 -0400

    Vite successfully working in Remix template

--- templates/remix/app/root.tsx
+++ templates/remix/app/root.tsx
@@ -1,17 +1,11 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
 import {
   Links,
-  LiveReload,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
-
-export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
-];
+} from '@remix-run/react';
+import './styles/global.css';
 
 export default function App() {
   return (
@@ -26,7 +20,6 @@ export default function App() {
         <Outlet />
         <ScrollRestoration />
         <Scripts />
-        <LiveReload />
       </body>
     </html>
   );
