6dce64e06bb6107c86e1ce99a8067b7ae1d6a635
jonathanprozzi <jonathanprozzi@gmail.com>
2024-05-01 10:19:03 -0400

    Adds in try/catch block and uses the onCancel to handle early termination via CTRL+C

--- index.ts
+++ index.ts
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 import * as fs from 'node:fs';
 import * as path from 'node:path';
-import { fileURLToPath } from 'node:url';
 import { cac } from 'cac';
 import prompts from 'prompts';
 import pc from 'picocolors';
@@ -19,62 +18,77 @@ cli
     const defaultTargetDir = 'my-hats-project';
     let targetDir;
 
-    if (projectDirectory === '.' || projectDirectory === '') {
-      // If no specific directory is provided, or it's the current directory, use the default directory name
-      const response = await prompts({
-        type: 'text',
-        name: 'dir',
-        message: 'Enter your project directory:',
-        initial: 'my-hats-project',
-      });
-
-      targetDir = path.resolve(process.cwd(), defaultTargetDir);
-    } else {
-      // Otherwise, resolve the provided directory name relative to the current working directory
-      targetDir = path.resolve(process.cwd(), projectDirectory);
-    }
-
-    console.log(`Creating project at: ${targetDir}`);
-
-    const projectName = path.basename(targetDir);
-
-    const questions = [
-      {
-        type: 'select',
-        name: 'template',
-        message: 'Choose a template to use',
-        choices: templates.map((t) => ({ title: t.display, value: t.name })),
-        initial: 0,
-      },
-    ];
-
-    const responses = await prompts(
-      questions as prompts.PromptObject<string>[]
-    );
-
-    if (!fs.existsSync(targetDir)) {
-      fs.mkdirSync(targetDir, { recursive: true });
-    }
-
-    options.template = responses.template;
-
-    const templateDir = path.resolve(
-      process.cwd(),
-      'templates',
-      options.template
-    );
-
-    console.log(`Template chosen: ${responses.template}`); // This will show what the user selected
-    console.log(`Template directory being checked: ${templateDir}`); // This will show the path being checked
-
-    if (!fs.existsSync(templateDir)) {
-      console.error(
-        `The requested template "${responses.template}" does not exist.`
-      );
-      return;
-    }
-
     try {
+      if (projectDirectory === '.' || projectDirectory === '') {
+        const response = await prompts(
+          {
+            type: 'text',
+            name: 'dir',
+            message: 'Enter your project directory:',
+            initial: 'my-hats-project',
+          },
+          {
+            onCancel: () => {
+              console.log(`Operation cancelled by user.`);
+              process.exit(1); // Exit with a non-zero status code
+            },
+          }
+        );
+
+        targetDir = path.resolve(
+          process.cwd(),
+          response.dir || defaultTargetDir
+        );
+      } else {
+        targetDir = path.resolve(process.cwd(), projectDirectory);
+      }
+
+      console.log(`Creating project at: ${targetDir}`);
+
+      const projectName = path.basename(targetDir);
+
+      const questions = [
+        {
+          type: 'select',
+          name: 'template',
+          message: 'Choose a template to use',
+          choices: templates.map((t) => ({ title: t.display, value: t.name })),
+          initial: 0,
+        },
+      ];
+
+      const responses = await prompts(
+        questions as prompts.PromptObject<string>[],
+        {
+          onCancel: () => {
+            console.log(`${pc.red('✖')} Operation cancelled`);
+            process.exit(1); // Exit with a non-zero status code
+          },
+        }
+      );
+
+      if (!fs.existsSync(targetDir)) {
+        fs.mkdirSync(targetDir, { recursive: true });
+      }
+
+      options.template = responses.template;
+
+      const templateDir = path.resolve(
+        process.cwd(),
+        'templates',
+        options.template
+      );
+
+      console.log(`Template chosen: ${responses.template}`);
+      console.log(`Template directory being checked: ${templateDir}`);
+
+      if (!fs.existsSync(templateDir)) {
+        console.error(
+          `The requested template "${responses.template}" does not exist.`
+        );
+        return;
+      }
+
       await fs.promises.cp(templateDir, targetDir, { recursive: true });
       console.log(`Project created successfully in ${targetDir}`);
     } catch (error) {
